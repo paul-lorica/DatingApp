c8a6350e047cfc9b4a8286b48608367c57c98a0a
Paul Lorica <plorica@3cloudsolutions.com>
2021-04-09 00:55:43 -0500

    login persistent

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-nav',
@@ -7,13 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
   model: any = {}
+  loggedIn: boolean;
 
-  constructor() { }
+  constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
+    this.getCurrentUser();
   }
 
   login() {
     console.log(this.model);
+
+    this.accountService.login(this.model).subscribe(response => {
+      console.log(response)
+      this.loggedIn = true;
+    }, error => {
+      console.log(error)
+    })
+  }
+
+  logout() {
+    this.accountService.logout();
+    this.loggedIn = false;
+  }
+
+  getCurrentUser() {
+    this.accountService.currentUser$.subscribe(user => {
+      this.loggedIn = !!user;
+    }, error => console.log(error));
   }
 }
